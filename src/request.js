503ff3f66a19b08bb5994ccc512817ea42e2ce67
Mark N Broadhead <MarkNBroadhead@gmail.com>
2022-11-10 13:07:44 -0700

    fix: node_env var evaluation

--- src/request.js
+++ src/request.js
@@ -1,16 +1,19 @@
 // ATTRIBUTION: https://github.com/dougmoscrop/serverless-http
 
+const { NODE_ENV } = process.env
+
 const http = require('http')
 
 const HTTPS_PORT = 443
+const HTTP_PORT = 80
 
 module.exports = class ServerlessRequest extends http.IncomingMessage {
   constructor ({ method, url, headers, body, remoteAddress }) {
     super({
-      encrypted: true,
+      encrypted: !(NODE_ENV === 'development'),
       readable: false,
       remoteAddress,
-      address: () => ({ port: HTTPS_PORT }),
+      address: () => ({ port: !(NODE_ENV === 'development') ? HTTP_PORT : HTTPS_PORT }),
       end: Function.prototype,
       destroy: Function.prototype
     })
